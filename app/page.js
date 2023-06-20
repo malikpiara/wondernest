3e991fafb01ba9d9e0f871609501913b400f0d03
Malik Piara <malikpiara@gmail.com>
2023-06-21 00:32:16 +0100

    Changed the blog headline.

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Wondernest</h1>
-      <h2>Hello for the sixth time</h2>
+      <h2>Sixth time's the charm</h2>
       <p>Welcome to latest iteration of my personal platform, Moonwith. This time built from scratch with Next.js 13 and the new app router.</p>
     </main>
   )
