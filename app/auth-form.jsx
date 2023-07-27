72bcd1e3b58afbb49bd3158c3dc02daa01d30f75
Malik Piara <malikpiara@gmail.com>
2023-07-27 16:54:43 +0100

    Started implementing signup with google and email. Made styles identical to Wondernest.

--- app/auth-form.jsx
+++ app/auth-form.jsx
@@ -9,11 +9,36 @@ export default function AuthForm() {
   return (
     <Auth
       supabaseClient={supabase}
-      view="magic_link"
-      appearance={{ theme: ThemeSupa }}
-      theme="light"
-      showLinks={false}
-      providers={[]}
+      view="sign_up"
+      appearance={{ theme: ThemeSupa,
+        variables: {
+          default: {
+            colors: {
+              brand: '#000000',
+              brandAccent: 'hsla(240 5.9% 10% / 0.9)',
+              inputBackground: 'white',
+              inputLabelText: 'black',
+              inputBorder: '#e5e7eb',
+            },
+            space: {
+              buttonPadding: '0.9rem 0.75rem',
+              inputPadding: '0.9rem 0.75rem',
+            },
+            fonts: {
+              bodyFontFamily: `inherit`,
+              buttonFontFamily: `inherit`,
+              inputFontFamily: `inherit`,
+              labelFontFamily: `inherit`,
+            },
+            radii: {
+              borderRadiusButton: '7px',
+              buttonBorderRadius: '7px',
+              inputBorderRadius: '7px',
+            },
+          },
+        }, }}
+      showLinks={true}
+      providers={['google']}
       redirectTo="http://wondernest.moonwith.com/auth/callback"
     />
   )
