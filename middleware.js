349f1a478f6c545bfa28c3c743fc88275b95904c
Malik Piara <malikpiara@gmail.com>
2023-07-28 16:55:30 +0100

    Redirecting logged in users from signup page to account page.

--- middleware.js
+++ middleware.js
@@ -9,19 +9,17 @@ export async function middleware(req) {
     data: { user },
   } = await supabase.auth.getUser()
 
-  // if user is signed in and the current path is / redirect the user to /account
-  /* if (user && req.nextUrl.pathname === '/') {
+  if (user && req.nextUrl.pathname === '/breakfast') {
     return NextResponse.redirect(new URL('/account', req.url))
-  } */
+  }
 
-  // if user is not signed in and the current path is not / redirect the user to /
-  if (!user && req.nextUrl.pathname !== '/') {
-    return NextResponse.redirect(new URL('/', req.url))
+  if (!user && req.nextUrl.pathname === '/account') {
+    return NextResponse.redirect(new URL('/breakfast', req.url))
   }
 
   return res
 }
 
 export const config = {
-  matcher: ['/', '/account'],
+  matcher: ['/', '/account', '/breakfast'],
 }
